ce7f02bfa43076d1034086accd73018385337307
Emil Martinov <martinov.emil@gmail.com>
2018-04-25 10:01:00 +0200

    Webpack updated to 4, broke the image imports & fixed it

---
+++ src/projects.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import project1 from './images/project1.jpg';
+import project2 from './images/project2.jpg';
+import project3 from './images/project3.jpg';
+import project4 from './images/project4.jpg';
+import project5 from './images/project5.jpg';
+import project6 from './images/project6.jpg';
+import svg from './images/github.svg';
+
+const projectPics = [project1, project2, project3, project4, project5, project6];
+
+class Projects extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return ( this.props.projects.map((project, i)=>
+                <div className='flex-projects' key={project.title}>
+                    <a href={project.url} target='_blank'>{project.title}</a>
+                        <p><br/>{project.description}<a className='github-icon' href={project.repo} target='_blank'>Github Repo<img className='github-icon' src={svg}/></a></p>
+                        <a href={project.url} target='_blank'><img src={projectPics[i]}/></a>
+                        {/* <a href={project.url} target='_blank'><img src={project.img}/></a> */}
+                </div>
+            )
+        )
+    }
+}
+
+export default Projects;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,50 +1,81 @@
-import {resolve} from 'path';
+import {resolve, join} from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import HtmlWebpackExternalsPlugin from 'html-webpack-externals-plugin';
 
-module.exports = env =>{
-    return({
-        context: resolve(__dirname, 'js'),
-        entry:[
-            './app.js',
-        ],
-        output:{
-            path: resolve('dist'),
-            filename:'app.bundle.js',
-            publicPath:'/dist'
-        },
-        module:{
-            rules:[
-                {
-                    test:/\.jsx?$/,
-                    exclude:/node_modules/,
-                    loader:'babel-loader'
-                },
-                {
-                    test:/\.css$/,
-                    use:[
-                        'style-loader',
-                        {
-                            loader:'css-loader',
-                            options:{
-                                minimize:true
-                            }
-                        },
-                        'postcss-loader'
-                    ]
-                },
-                {
-                    test:/\.(jpeg|png|svg|gif)$/,
-                    use:[
-                        {
-                            loader:'file-loader',
-                            options:{}
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + 'dist/',
+        filename: 'app.bundle.js'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
                         }
-                    ]
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:[
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(svg|jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src','img:src']
+                    }
                 }
-            ]
-        },
-        devServer:{
-            compress:true,
-            open:true
-        }
-    });
+            }
+        ]
+    },
+    devServer: {
+        compress: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+          title: 'Development',
+          template: './src/index.html' // Normally creates a generic HTML for js file. But with template we can feed in our own HTML.
+        }),
+        // new HtmlWebpackExternalsPlugin({
+        //   externals: [
+        //     {
+        //       module:'jquery', //Example for CDN
+        //       entry: 'https://unpkg.com/jquery@3.2.1/dist/jquery.min.js',
+        //       // global:'jQuery'  //if you want to setup a global
+        //     }
+        //   ]
+        // })
+    ]
 }
